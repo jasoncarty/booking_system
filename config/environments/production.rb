b91ecc2bde3b7fd865cfe2da82c061cc5fdb0b84
Jason Carty <jasoncarty@Jasons-MacBook-Pro.local>
2017-09-19 20:15:49 +0200

    changes smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'golf-booking-system.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.live.com',
+    address:              'smtp.gmail.com',
     port:                 587,
     domain:               'golf-booking-system.herokuapp.com',
     user_name:            ENV['mail_email'],
